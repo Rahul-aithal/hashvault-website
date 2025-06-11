40b6294ed244bfc8111112597801325f5cf7f4ca
Abhiram H <Abhiramh21@gmail.com>
2025-06-11 21:07:11 +0530

    feat: Add Markdoc configuration and content structure for HashVault CTF documentation
    - Created markdoc.config.json and markdoc.config.mjs to define Markdoc settings and custom nodes.
    - Implemented a custom Heading component in Heading.astro for enhanced heading rendering.
    - Established content configuration in content.config.ts for managing documentation collections.
    - Added initial documentation content in welcome.mdoc outlining the HashVault CTF initiative.
    - Developed dynamic documentation pages in [id].astro to render content based on collection entries.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
+// import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
 
 import alpinejs from "@astrojs/alpinejs";
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), alpinejs()],
+  integrations: [alpinejs(), markdoc()],
 
   vite: {
+    css: {
+      transformer: "lightningcss",
+    },
     plugins: [tailwindcss()],
   },
 });
