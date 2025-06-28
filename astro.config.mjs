c8e77ec761e44cf6b8bc9c889c95814068521ebc
Abhiram H <Abhiramh21@gmail.com>
2025-06-28 20:42:22 +0530

    Deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hashvault.club',
   integrations: [
     alpinejs(),
     markdoc(),
